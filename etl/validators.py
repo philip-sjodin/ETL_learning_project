c4ba1d63233a06599f153a50fbdd29a54c9953ba
Philip Sjödin <sjodinphilip@gmail.com>
2026-02-08 16:23:35 +0100

    feat(etl): add validators for schema and primary keys

---
+++ etl/validators.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def assert_required_colums(df: pd.DataFrame, required: set[str], table_name: str) -> None:
+    missing = required - set(df.columns)
+    if missing:
+        raise ValueError(f"{table_name}: missing required columns: {missing}")
+    
+
+def assert_pk_not_null(df: pd.DataFrame, pk: str, table_name: str) -> None:
+    if df[pk].isna().any():
+        raise ValueError(f"{table_name}: primary key '{pk}' contains NULL values")
+    
+
+def assert_pk_unique(df: pd.DataFrame, pk: str, table_name: str) -> None:
+    if not df[pk].is_unique:
+        duplicates = df[pk][df[pk].duplicated()].unique()
+        raise ValueError(f"{table_name}: does not have unique primary key '{pk}': \n{duplicates}")
